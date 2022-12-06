8b3df54d5e75ce83510798c3b53c519e6e9475a3
BrunaAlmeida13 <bruna.a.almeida@hotmail.com>
2022-12-05 23:37:11 -0300

    Configurando projeto

---
+++ src/main/java/br/edu/infnet/gerenciadordecomodos/GerenciadorDeComodosApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.gerenciadordecomodos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorDeComodosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorDeComodosApplication.class, args);
+	}
+
+}
